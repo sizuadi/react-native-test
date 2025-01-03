d8d3b45e416668fb81443098256c292ad3270a35
sizuadi <adi15siswanto@gmail.com>
2025-01-03 23:36:53 +0700

    feat: just edit small things, because running out of time :'3

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>This is me M Adi Siswanto start to learn react native :)</Text>
     </View>
   );
 }
