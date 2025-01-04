a66d293070bd9a460a746c7e6dab000d709366db
sizuadi <adi15siswanto@gmail.com>
2025-01-04 22:21:38 +0700

    update text

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
 	return (
 		<View style={styles.container}>
 			<View>
-				<Text>Hello World!!! Test</Text>
+				<Text style={styles.dummyText}>Another piece of text!</Text>
 			</View>
+			<Text style={styles.dummyText}>Hello World!!!</Text>
+			<Button title='Press me' />
 			<StatusBar style='auto' />
 		</View>
 	);
@@ -18,5 +20,12 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	dummyText: {
+		margin: 16,
+		padding: 16,
+		borderWidth: 2,
+		borderColor: 'red',
+		borderRadius: 8,
 	},
 });
