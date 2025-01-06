183f4e607156f5afb614918d1051ca16167f0385
Adi Siswanto <adi15siswanto@gmail.com>
2025-01-06 18:25:08 +0700

    add list and showing list

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 export default function Index() {
+	const [enteredGoalText, setEnteredGoalText] = useState('');
+	const [courseGoals, setCourseGoals] = useState<string[]>([]);
+
+	function goalInputHandler(enteredText: string) {
+		setEnteredGoalText(enteredText);
+	}
+
+	function addGoalHandler() {
+		setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
+	}
 	return (
 		<View style={styles.appContainer}>
 			<View style={styles.inputContainer}>
-				<TextInput style={styles.textInput} placeholder='Your course goal!' />
-				<Button title='Add Goal' />
+				<TextInput
+					style={styles.textInput}
+					placeholder='Your course goal!'
+					onChangeText={goalInputHandler}
+				/>
+				<Button title='Add Goal' onPress={addGoalHandler} />
 			</View>
-			<View>
-				<Text style={styles.dummyText}>Hello World!!!</Text>
+			<View style={styles.listInputContainer}>
+				<ScrollView>
+					{courseGoals.map((goal, index) => (
+						<View style={styles.goalItem} key={index}>
+							<Text style={styles.goalText}>{goal}</Text>
+						</View>
+					))}
+				</ScrollView>
 			</View>
 			<StatusBar style='auto' />
 		</View>
@@ -22,21 +43,34 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		padding: 30,
 	},
-	dummyText: {
-		margin: 16,
+	goalItem: {
 		padding: 16,
-		borderWidth: 2,
-		borderColor: 'red',
+		marginBottom: 12,
+		backgroundColor: '#004e70',
 		borderRadius: 8,
+	},
+	goalText: {
+		color: '#ededed',
 	},
 	inputContainer: {
 		flexDirection: 'row',
 		justifyContent: 'space-between',
+		flex: 1,
+		alignItems: 'center',
+		marginBottom: 24,
+		borderBottomWidth: 2,
+		borderBottomColor: '#cccccc',
+	},
+	listInputContainer: {
+		flexDirection: 'column',
+		justifyContent: 'flex-start',
+		flex: 5,
 	},
 	textInput: {
 		borderWidth: 1,
 		borderColor: '#cccccc',
 		width: '70%',
 		marginRight: 8,
+		borderRadius: 8,
 	},
 });
